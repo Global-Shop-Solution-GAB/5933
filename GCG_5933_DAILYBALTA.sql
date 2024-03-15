cb01d84658edab544a3eb7c5d9564878678a1b54
Service Web <sw@gssmail.com>
2024-03-15 09:05:26 -0500

    Commit changes in C:\Websites\Git\emarlow\5933\GCG_5933_DAILYBALTA.sql

---
+++ GCG_5933_DAILYBALTA.sql
@@ -0,0 +1,11 @@
+CREATE TABLE "GCG_5933_DailyBalTA" 
+ (
+ "Run_Sunday" BIT NOT NULL,
+ "Run_Monday" BIT NOT NULL,
+ "Run_Tuesday" BIT NOT NULL,
+ "Run_Wednesday" BIT NOT NULL,
+ "Run_Thursday" BIT NOT NULL,
+ "Run_Friday" BIT NOT NULL,
+ "Run_Saturday" BIT NOT NULL,
+ "Employee_Status" CHAR(50) );
+
